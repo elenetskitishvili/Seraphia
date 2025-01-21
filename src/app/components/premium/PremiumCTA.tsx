736c290159475844d9f4eae96c5f6ed33b35f464
elenetskitishvili <elene.tskitishvili504@gmail.com>
2025-01-21 11:17:00 +0400

    style: make premium cta component more responsive

--- src/app/components/premium/PremiumCTA.tsx
+++ src/app/components/premium/PremiumCTA.tsx
@@ -7,19 +7,21 @@ export default function PremiumCTA() {
       <MarqueeText />
 
       {/* CONTENT */}
-      <div className="bg-customBlue text-center py-[60px] 480px:py-20 770px:py-[120px] px-6">
-        <p className="text-white text-lg 480px:text-xl mb-6 font-semibold">
-          Receive deals every week, no spam
-        </p>
-        <h1 className="text-[28px] 480px:text-4xl 770px:text-5xl 990px:text-[64px] font-bold leading-none -tracking-[1px] text-white mb-14 990px:mb-10">
-          Subscribe the newsletter and save 20% on your first order!
-        </h1>
-        <Link
-          href="/premium"
-          className="w-full 480px:w-auto 480px:px-[50px] text-customBlue bg-white font-bold py-3  rounded-full inline-block mb-7"
-        >
-          Go Premium!
-        </Link>
+      <div className="bg-customBlue text-center py-[60px] 480px:py-20 770px:py-[120px] px-6 770px:px-[70px]">
+        <div className=" max-w-screen-xl mx-auto">
+          <p className="text-white text-lg 480px:text-xl mb-6 font-semibold">
+            Receive deals every week, no spam
+          </p>
+          <h1 className="text-[28px] 480px:text-4xl 770px:text-5xl 990px:text-[64px] font-bold leading-none -tracking-[1px] text-white mb-14 990px:mb-10">
+            Subscribe the newsletter and save 20% on your first order!
+          </h1>
+          <Link
+            href="/premium"
+            className="w-full 480px:w-auto 480px:px-[50px] text-customBlue bg-white font-bold py-3  rounded-full inline-block mb-7"
+          >
+            Go Premium!
+          </Link>
+        </div>
       </div>
     </section>
   );
