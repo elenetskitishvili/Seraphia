391ce5ce806fd3e7260627605ee0eae2fed2d086
elenetskitishvili <elene.tskitishvili504@gmail.com>
2025-01-23 23:25:10 +0400

    feat: add platform tagline on home page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -4,6 +4,13 @@ export default function Home() {
   return (
     <>
       <HomeHero />
+      <section className=" px-6 770px:px-10 pt-[60px] 480px:pt-20 770px:pt-[120px] 990px:pt-[160px] pb-10 770px:pb-[60px] 990px:pb-20 ">
+        <h2 className="text-[28px] 480px:text-[36px] 990px:text-[64px] 770px:text-5xl text-center font-bold tracking-tighter max-w-[1060px] mx-auto leading-none">
+          Shopverse is a jewelry platform to shop, sell, and explore blogs &
+          stories.
+        </h2>
+      </section>
+      <section></section>
       <PremiumCTA />
     </>
   );
