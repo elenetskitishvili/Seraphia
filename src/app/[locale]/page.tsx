4e4cdff0c316a0ae6a3df939626d4b13db1edbaa
elenetskitishvili <elene.tskitishvili504@gmail.com>
2025-01-24 13:19:37 +0400

    feat: add small features section on the homepage

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,5 +1,12 @@
 import HomeHero from "@/src/app/components/home/HomeHero";
 import PremiumCTA from "../components/premium/PremiumCTA";
+
+import {
+  HeartIcon,
+  TagIcon,
+  ShieldCheckIcon,
+} from "@heroicons/react/24/outline";
+
 export default function Home() {
   return (
     <>
@@ -10,7 +17,32 @@ export default function Home() {
           stories.
         </h2>
       </section>
-      <section></section>
+      <section className="flex items-center justify-center border-y border-y-bgBtn">
+        <div className="flex flex-col 480px:flex-row gap-10 480px:gap-6 items-center 480px:justify-between  text-center px-6 py-16 font-bold text-customGray">
+          <div className="flex flex-col items-center justify-center gap-3 max-w-[290px]">
+            <div className="w-14 h-14 rounded-full bg-bgLight flex items-center justify-center ">
+              <HeartIcon className="w-8 h-8  text-customBlue" />
+            </div>
+            <p>
+              Our customers love Shopverse user interface and user experience
+            </p>
+          </div>
+          <div className="flex flex-col items-center justify-center gap-3 max-w-[290px]">
+            <div className="w-14 h-14 rounded-full bg-bgLight flex items-center justify-center">
+              <TagIcon className="w-8 h-8 text-customBlue" />
+            </div>
+            <p>Your go-to store to find the best, most exclusive items</p>
+          </div>
+          <div className="flex flex-col items-center justify-center gap-3 max-w-[290px]">
+            <div className="w-14 h-14 rounded-full bg-bgLight flex items-center justify-center ">
+              <ShieldCheckIcon className="w-8 h-8  text-customBlue" />
+            </div>
+            <p>
+              Our products are high quality, we have over 10.000 google reviews
+            </p>
+          </div>
+        </div>
+      </section>
       <PremiumCTA />
     </>
   );
