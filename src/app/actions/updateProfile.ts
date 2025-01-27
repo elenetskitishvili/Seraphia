54266114abe9ba279224984dcea45bff7d982532
elenetskitishvili <elene.tskitishvili504@gmail.com>
2025-01-27 20:42:25 +0400

    feat: implement profile update functionality

--- src/app/actions/updateProfile.ts
+++ src/app/actions/updateProfile.ts
@@ -1,4 +1,46 @@
+"use server";
+
+import { createClient } from "@/src/utils/supabase/server";
+import { getLocale } from "next-intl/server";
+
 export async function updateProfile(formData: FormData) {
-  await new Promise((resolve) => setTimeout(resolve, 3000));
-  console.log("Profile updated!🤩");
+  const supabase = await createClient();
+  const locale = await getLocale();
+
+  const fullName = formData.get("fullName") as string;
+  const phone = formData.get("phone") as string;
+  const makesJewelry = formData.get("makesJewelry") === "on";
+  const address = formData.get("address") as string;
+
+  try {
+    const userResponse = await supabase.auth.getUser();
+    const userId = userResponse.data.user?.id;
+
+    if (!userId) throw new Error("User not authenticated.");
+
+    const { data, error } = await supabase
+      .from("users")
+      .update({
+        full_name: fullName,
+        phone: phone,
+        makes_jewelry: makesJewelry,
+        address: address,
+      })
+      .eq("user_id", userId)
+      .select()
+      .single();
+
+    console.log("data:🤩", data);
+    console.log("error:🤩", error);
+
+    if (error) {
+      throw new Error("Failed to update user in Supabase.");
+    }
+
+    return data;
+  } catch (error) {
+    console.error("Error updating user information in Supabase:", error);
+
+    throw error;
+  }
 }
