b4ed95170b349b3a8066e0be1844233f91b9c0c3
elenetskitishvili <elene.tskitishvili504@gmail.com>
2025-02-01 15:16:51 +0400

    style: add dark mode colors to the premium cta component

--- src/components/premium/MarqueeText.tsx
+++ src/components/premium/MarqueeText.tsx
@@ -2,10 +2,24 @@
 
 import { motion } from "framer-motion";
 import Image from "next/image";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 export default function MarqueeText() {
+  const { theme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  const logoSrc =
+    theme === "dark" ? "/svg/logo-white.svg" : "/svg/logo-blue.svg";
+
   return (
-    <div className="overflow-hidden whitespace-nowrap flex py-8 bg-bgMedium">
+    <div className="overflow-hidden whitespace-nowrap flex py-8 bg-bgMedium dark:bg-darkModeBg">
       <motion.div
         className="flex gap-10 text-lg font-semibold"
         animate={{ x: ["0%", "-100%"] }}
@@ -13,15 +27,10 @@ export default function MarqueeText() {
       >
         {[...Array(50)].map((_, i) => (
           <span key={i} className="flex items-center gap-6">
-            <span className="text-customBlue  text-2xl">
+            <span className="text-customBlue dark:text-darkModeText text-2xl">
               Upgrade to Premium
             </span>
-            <Image
-              src="/svg/logo-blue.svg"
-              alt="blue logo"
-              width={24}
-              height={24}
-            />
+            <Image src={logoSrc} alt="blue logo" width={24} height={24} />
           </span>
         ))}
       </motion.div>
--- src/components/premium/PremiumCTA.tsx
+++ src/components/premium/PremiumCTA.tsx
@@ -7,17 +7,17 @@ export default function PremiumCTA() {
       <MarqueeText />
 
       {/* CONTENT */}
-      <div className="bg-customBlue text-center py-[60px] 480px:py-20 770px:py-[120px] px-6 770px:px-[70px]">
+      <div className="bg-customBlue dark:bg-indigo-800 text-center py-[60px] 480px:py-20 770px:py-[120px] px-6 770px:px-[70px]">
         <div className=" max-w-screen-xl mx-auto">
-          <p className="text-white text-lg 480px:text-xl mb-6 font-semibold">
+          <p className="text-white dark:text-darkModeText text-lg 480px:text-xl mb-6 font-semibold">
             Receive deals every week, no spam
           </p>
-          <h1 className="text-[28px] 480px:text-4xl 770px:text-5xl 990px:text-[64px] font-bold leading-none tracking-tighter text-white mb-14">
+          <h1 className="text-[28px] 480px:text-4xl 770px:text-5xl 990px:text-[64px] font-bold leading-none tracking-tighter text-white dark:text-darkModeText mb-14">
             Subscribe the newsletter and save 20% on your first order!
           </h1>
           <Link
             href={"/pricing"}
-            className="btn relative inline-block rounded-full transition-all duration-200 px-20 py-[15px] bg-white text-[#777] hover:-translate-y-[3px] active:-translate-y-[1px] hover:shadow-[0_10px_20px_rgba(0,0,0,0.2)] active:shadow-[0_5px_10px_rgba(0,0,0,0.2)]"
+            className="btn relative inline-block rounded-full transition-all duration-200 px-20 py-[15px] bg-white dark:bg-bgLighter font-medium text-customDark hover:-translate-y-[3px] active:-translate-y-[1px] hover:shadow-[0_10px_20px_rgba(0,0,0,0.2)] active:shadow-[0_5px_10px_rgba(0,0,0,0.2)]"
           >
             Go Premium!
           </Link>
